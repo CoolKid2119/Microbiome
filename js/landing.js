f845e3d43664f13f4393d16a4b6967db5849d34e
Audy Lebovitz <audylebovitz@audys-mbp.myfiosgateway.com>
2020-04-21 21:58:40 -0400

    relevant message

--- js/landing.js
+++ js/landing.js
@@ -1,3 +1,8 @@
 function showVideo() {
   document.getElementById("video").style.display = "block"; 
 }
+
+document.getElementById('video').addEventListener('ended',myHandler,false);
+    function myHandler(e) {
+    	location.href='https://coolkid2119.github.io/Microbiome/plate.html';
+    }
