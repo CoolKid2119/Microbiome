fe9a7c9271f539e2ab5520c0bf23fea21381ee6f
Audy Lebovitz <audylebovitz@audys-mbp.myfiosgateway.com>
2020-04-22 08:15:45 -0400

    relevant message

--- js/landing.js
+++ js/landing.js
@@ -1,8 +1,24 @@
 function showVideo() {
-  document.getElementById("video").style.display = "block"; 
-}
+	var elem = document.getElementById("video");
+  	//document.getElementById("video").style.display = "block";
 
-document.getElementById('video').addEventListener('ended',myHandler,false);
-    function myHandler(e) {
-    	location.href='https://coolkid2119.github.io/Microbiome/plate.html';
-    }
+	if (elem.requestFullscreen) {
+		elem.requestFullscreen();
+	}
+	else if (elem.mozRequestFullScreen) {
+		elem.mozRequestFullScreen();
+	}
+	else if (elem.webkitRequestFullscreen) {
+		elem.webkitRequestFullscreen();
+	}
+	else if (elem.msRequestFullscreen) { 
+		elem.msRequestFullscreen();
+	}
+
+	elem.play();
+
+	document.getElementById("video").addEventListener('ended',myHandler,false);
+		function myHandler(e) {
+			location.href='file:///Users/audylebovitz/Desktop/Microbiome/plate.html';
+    	}
+}
