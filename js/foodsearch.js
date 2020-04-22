4da53d7695a31ca9d52cdc90d1ee7a26384b4b4f
Audy Lebovitz <audylebovitz@audys-mbp.myfiosgateway.com>
2020-04-22 13:21:39 -0400

    relevant message

--- js/foodsearch.js
+++ js/foodsearch.js
@@ -2,10 +2,12 @@ function addTag(event){
 	var name = $(event.target.outerHTML).attr("value");
 	$(".typing").html('<span class="fgtext" >' + name +  '</span>' + $(".typing").html());
 	$(".textbox").focus();
+	$(".fa-times-circle").show();
 }
 
 $(document).on('click', '.fgimg', addTag);
 
 function bet(){
 	console.log('bet');
+	$(".fa-times-circle").show();
 }
