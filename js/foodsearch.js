150d4ef927e54812b0cd1584784a8dbf8d0d4af8
Audy Lebovitz <audylebovitz@audys-mbp.myfiosgateway.com>
2020-04-24 12:25:54 -0400

    relevant message

--- js/foodsearch.js
+++ js/foodsearch.js
@@ -3,22 +3,36 @@ $(document).ready(function () {
 
 	$('.search').on('input', function() {
 		console.log($(this).children());
-		var value = $(this).children()[1].children[0].value;
-		var length = value.length;
-		if(length == 0){
-			$(".fa-times-circle").hide();
+		var first_child = $(this).children()[1].children[0];
+		if("type" in first_child){
+			var value = first_child.value; /* when a tag is there that will be the first child, not the input */
+			var length = value.length;
+			if(length == 0){
+				$(".fa-times-circle").css("visibility", "hidden");
+			}
+			if(length == 1){
+				$(".fa-times-circle").css("visibility", "visible");
+			}
 		}
-		if(length == 1){
-			$(".fa-times-circle").show();
+		else {
+			$(".fa-times-circle").css("visibility", "visible");
+			console.log("breh");
 		}
+		
+	});
+
+	$('input').on('itemRemoved', function(event) {
+		$(".fa-times-circle").css("visibility", "hidden");
 	});
 
 	$('#SearchString').tagsinput({
-		itemValue: 'text'
+		itemValue: 'text',
+		allowDuplicates: true
 	});
 
 	var so = $(".fa-times-circle").width() / $('.search').width() * 100;
-   $(".parent-circle").css("width", so + "%");
+   $(".parent-circle").css("width", $(".fa-times-circle").width()  + "px");
+   /*$(".parent-circle").css("width", so + "%");*/
 
 
    var f = $(".fa-search").width() / $('.fa-search').parent().width() * 100;
@@ -37,6 +51,34 @@ $(document).ready(function () {
 
 });
 
+function clearText(){
+	$(".fa-times-circle").css("visibility", "hidden");
+	$(".bootstrap-tagsinput input").val('').focus();
+
+	$(".bootstrap-tagsinput span").remove();
+
+	var so = $(".fa-times-circle").width() / $('.search').width() * 100;
+   $(".parent-circle").css("width", so + "%");
+
+
+   var f = $(".fa-search").width() / $('.fa-search').parent().width() * 100;
+   $('.bootstrap-tagsinput').css('width', (100-f-so) + '%');
+	/*$(".bootstrap-tagsinput").filter(function() {
+		var $this = $(this);
+		return $this.css("display") == "none";
+	}).remove();*/
+
+	$(".bootstrap-tagsinput input").css("width", "100%");
+	/*$(".bootstrap-tagsinput input").attr("placeholder", "Enter food item");*/
+
+
+	/*console.log($(".bootstrap-tagsinput").tagsinput('items'));
+	$(".bootstrap-tagsinput").tagsinput("remove", { id: 'tag', text: "Vegetables" });*/
+	/*$(".bootstrap-tagsinput input").css("width", "100%");*/
+
+
+}
+
 function translateField(){
 	//$(".fa-times-circle").show(); nope only do this when they have typing something
 	// todo translate the whole div
@@ -45,9 +87,14 @@ function translateField(){
 	/*$(".bootstrap-tagsinput input").focus();*/
 	/*$(".search").css("width", "80%");*/
 	var translation = 2 * $(".title").height() + 0.07 * $(".titlesearch").parent().height();
+	console.log(translation);
+	console.log('bet');
 	var text = "translateY(-" + translation + "px)";
 	var f = $(".fa-search").width() * 1.25 / $('.fa-search').parent().width() * 100;
-	$('.bootstrap-tagsinput').css('width', (100-f) + '%');
+
+	var so = $(".fa-times-circle").width() * 1.25 / $('.search').width() * 100;
+
+	$('.bootstrap-tagsinput').css('width', (100-f-so) + '%');
 	$(".search").css("width", "80%");
 	$(".cancel").css("width", "14%");
 
@@ -55,10 +102,34 @@ function translateField(){
 	$(".line").css("transform", text);
 	$(".category").css("transform", text);
 	$("#table").css("transform", text);
+}
+
+function search_home(){
+
+	$(".bootstrap-tagsinput span").remove();
+	$(".bootstrap-tagsinput input").css("width", "100%");
+	$(".bootstrap-tagsinput input").val('');
+	$(".fa-times-circle").css("visibility", "hidden");
+
+	var translation = 2 * $(".title").height() - 4 * 0.07 * $(".titlesearch").parent().height();
+	console.log(translation);
+	console.log("lmfao");
+	var text = "translateY(" + translation + "px)";
+	var f = $(".fa-search").width() / $('.fa-search').parent().width() * 100; /* uhh why did we need 1.25 above and not here */
+
+	var so = $(".fa-times-circle").width() / $('.search').width() * 100;
+
+	$(".search").css("width", "94%");
+	$(".cancel").css("width", "0%");
+	$('.bootstrap-tagsinput').css('width', (100-f-so) + '%');
+
+	$(".titlesearch").css("transform", text);
+	$(".line").css("transform", text);
+	$(".category").css("transform", text);
+	$("#table").css("transform", text);
 
 
-
-
+	
 }
 
 function addTag(event){
@@ -67,9 +138,24 @@ function addTag(event){
 
 	$('SearchString').attr('placeholder', '');
 
+
 	translateField();
 
-	$(".fa-times-circle").show();
+	/*$('.tag').css("display", "inline-block");
+	$('.tag').css("float", "left");
+	$('.bootstrap-tagsinput input').css("display", "inline-block");*/
+
+	var parentWidth = $('.bootstrap-tagsinput input').parent().width();
+	var width = $('.tag').width();
+	console.log($('.bootstrap-tagsinput input').parent());
+	console.log(parentWidth);
+	console.log(width);
+	$('.bootstrap-tagsinput input').css("width", parentWidth - width);
+
+	$(".bootstrap-tagsinput input").val('').focus();
+	$(".fa-times-circle").css("visibility", "visible");
+
+
 }
 
 $(document).on('click', '.fgimg', addTag);
